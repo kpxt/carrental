b71007332e6fae27930b2df8757efee70b666083
Niyant Shah <niyantshah02@gmail.com>
2020-06-16 02:49:22 +0530

    Implemented Contact form and booking php script

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_USER', 'niyantshah2602@gmail.com');
+define('MAIL_PASS', 'mr.black94296');
+define('MAIL_SECURE', PHPMailer::ENCRYPTION_STARTTLS);
+define('MAIL_PORT', 587);
