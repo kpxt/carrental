2a30f500675ad82a877bd57a3f51e22d5db6614e
Niyant Shah <33359487+niyant02@users.noreply.github.com>
2020-06-16 02:55:04 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 
 define('MAIL_HOST', 'smtp.gmail.com');
-define('MAIL_USER', 'niyantshah2602@gmail.com');
-define('MAIL_PASS', 'mr.black94296');
+define('MAIL_USER', '');
+define('MAIL_PASS', '');
 define('MAIL_SECURE', PHPMailer::ENCRYPTION_STARTTLS);
 define('MAIL_PORT', 587);
